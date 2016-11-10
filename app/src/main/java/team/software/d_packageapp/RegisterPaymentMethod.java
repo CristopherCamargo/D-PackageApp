72df11a46e4e27ff1b06bba65cb352067c2d6349
CristopherCamargo <cristopher.cc92@gmail.com>
2016-11-09 21:01:40 -0400

    Interfaces Spring3 backlog 1

---
+++ app/src/main/java/team/software/d_packageapp/RegisterPaymentMethod.java
@@ -0,0 +1,13 @@
+package team.software.d_packageapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterPaymentMethod extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_payment_method);
+    }
+}
---
+++ app/src/main/java/team/software/d_packageapp/RequestPackage.java
@@ -0,0 +1,13 @@
+package team.software.d_packageapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequestPackage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_package);
+    }
+}
